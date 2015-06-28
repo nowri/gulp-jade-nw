e87a46229f71b4f25e1dc179d36d3ef09e4eda54
nowri <interactive@nowri.in>
2015-06-28 19:10:58 +0900

    Add Jade config

--- gulpfile.js
+++ gulpfile.js
@@ -6,50 +6,60 @@ var watch = require('gulp-watch');
 var runSequence = require('run-sequence');
 var del = require('del');
 var dirs = {
-	deploy	:	'deploy',
-	release	:	'_release',
-	src		:	'src'
+    deploy: 'deploy',
+    release: '_release',
+    src: 'src'
 };
 
-gulp.task('clean:allRelease', function(cb) {
-	del([dirs.release], cb);
+gulp.task('clean:allRelease', function (cb) {
+    del([dirs.release], cb);
 });
 
-gulp.task('copy:toRelease', function() {
-	return gulp
-		.src(
-			[dirs.deploy + '/**'],
-			{base: dirs.deploy}
-		)
-		.pipe(gulp.dest(dirs.release));
+gulp.task('copy:toRelease', function () {
+    return gulp
+        .src(
+        [dirs.deploy + '/**'],
+        {base: dirs.deploy}
+    )
+        .pipe(gulp.dest(dirs.release));
 });
 
 gulp.task('watch', function () {
-	gulp.watch(dirs.src +  '/**', ['deploy']);
+    gulp.watch(dirs.src + '/**', ['deploy']);
 });
 
 /*
  * Custom setting
  * */
-gulp.task('common:before', function(cb) {
-	runSequence(
-		/* Common tasks here */
-		cb);
+var jade = require('gulp-jade');
+
+gulp.task('jade', function () {
+    gulp.src(dirs.src + '/jade/*.jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest(dirs.deploy));
 });
 
-gulp.task('deploy', function(cb) {
-	runSequence('common:before',
-		/* Deploy tasks here */
-		cb);
+gulp.task('common:before', function (cb) {
+    runSequence(
+        'jade',
+        cb);
 });
 
-gulp.task('release', function(cb) {
-	runSequence(
-		'common:before',
-		'clean:allRelease',
-		'copy:toRelease',
-		/* Release tasks here */
-		cb);
+gulp.task('deploy', function (cb) {
+    runSequence('common:before',
+        /* Deploy tasks here */
+        cb);
+});
+
+gulp.task('release', function (cb) {
+    runSequence(
+        'common:before',
+        'clean:allRelease',
+        'copy:toRelease',
+        /* Release tasks here */
+        cb);
 });
 
 gulp.task('default', ['release']);
